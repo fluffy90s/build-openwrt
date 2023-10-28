13d59ba8f87f00cc63b2d9227197bd8e8b605a5f
hui <chicken.19900726@gmail.com>
2023-10-29 00:55:50 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -8,7 +8,11 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
-
+#mkdir -p -m 777 files/etc/config
+#echo -e "config global\n        option anon_mount '0'\n        option auto_swap '1'\n        option auto_mount '1'
+#        option delay_root '5'\n        option check_fs '1'\n        option anon_swap '0'\n" > files/etc/config/fstab
